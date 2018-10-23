7e615a57c54a27f2134ab2f0991a56b9e0ee5b0c
Danny Fekete <dannyfekete@gmail.com>
2018-10-23 14:19:59 -0400

    Massive refactor: We've moved message-sending functionality out to a module, and now both Room and Server may call it as needed.  Everything still works!  (Thanks, Tim Johns, for hand-holding during this surgery. - Danny, Alfred, Emily.

--- server/game/Room.js
+++ server/game/Room.js
@@ -1,13 +1,10 @@
 const Player = require('./Player.js');
-// const {broadcast} = require('../server.js');
-
 
 class Room {
 
-  constructor () {
-    this.hello = "You found me.",
-    this.players = []
-    // broadcast()
+  constructor (messager) {
+    this.messager = messager;
+    this.players = [];
   }
 
   //  This could be a class method, rather than an instance method.
@@ -19,16 +16,16 @@ class Room {
     }
   }
 
-  playerJoin (playerObject, broadcastMessage, sendClientMessage) {
+  playerJoin (protoPlayerObject) {
     // console.log(JSON.stringify(playerObject));
-    const newPlayer = new Player(playerObject);
+    const newPlayer = new Player(protoPlayerObject);
     this.players.push(newPlayer);
     // const message = this.clientNotifier("Hello from DEEP INSIDE THE GAME.");
     // message.id = playerObject.id;
     // console.log(JSON.stringify(message));
     console.log(`sendClientMessage to ${newPlayer.clientId}`);
-    sendClientMessage(this.serverMessageFormatter({message: "Hello from deep in the game!"}, newPlayer.clientId, "incomingNewPlayer"));
-    broadcastMessage(this.serverMessageFormatter({message: `New player, ${newPlayer.handle}, has joined!`}, newPlayer.clientId, "incomingNewPlayer"), true)
+    this.messager.sendClientMessage(this.serverMessageFormatter({message: "Hello from deep in the game!"}, newPlayer.clientId, "incomingNewPlayer"));
+    this.messager.broadcastMessage(this.serverMessageFormatter({message: `New player, ${newPlayer.handle}, has joined!`}, newPlayer.clientId, "incomingNewPlayer"), true)
 
     console.log(`Game says: my players are now ${this.players}`);
   }
---
+++ server/message-functions.js
@@ -0,0 +1,57 @@
+const WebSocket = require('ws');
+const uuidv4 = require('uuid/v4');
+
+
+class Messager {
+
+  constructor (wss) {
+    this.wss = wss;
+  }
+
+  // Fully processes a message: takes the initial object, applies a unique identifier, validates its type, and sends it out for broadcasts to all connected users, or all users excepting the provided socket (in the argument, othersOnly).
+
+  broadcastMessage (messageObject, othersOnly) {
+    // If the messageObject hasn't already been stamped with its own id, stamp it now.  This would only be true in the case of a message sent by the game, not by a user.
+    messageObject.id = messageObject.id || uuidv4();
+
+    console.log(`The value of othersOnly is ${othersOnly}`);
+
+    (othersOnly) ? this.broadcastOthers(messageObject) : this.broadcast(messageObject);
+  }
+
+  // Delivers the message object to all connected users.
+  broadcast (messageObject) {
+    console.log("You shouldn't see me.")
+      this.wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify(messageObject));
+      }
+    });
+  }
+
+  // Delivers the message object to all connected users EXCEPT the triggering user.
+  broadcastOthers (messageObject) {
+      this.wss.clients.forEach((client) => {
+        console.log(`BroadcastOthers is getting a message with client id ${client.clientId}`)
+      if (client.clientId !== messageObject.clientId && client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify(messageObject));
+      }
+    });
+  }
+
+  // Sends a message to one particular Client.
+
+  sendClientMessage (messageObject) {
+    this.wss.clients.forEach(function each(client) {
+      // console.log(`client.id: ${client.id}\nmessageObject.content.id: ${messageObject.content.id}`);
+      if (messageObject.clientId === client.clientId && client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify(messageObject))
+        // console.log(messageObject.content.message);
+      }
+    });
+  }
+
+}
+
+
+module.exports = Messager;
