fb2ff3cb40de448f347bf7c129923be1b469b3df
Alfred Law <alfred.lawsn@gmail.com>
2018-10-25 15:32:41 +0000

    added passport api into server and added login form

--- server/server.js
+++ server/server.js
@@ -8,6 +8,37 @@ const WebSocket = require('ws');
 const uuidv4 = require('uuid/v4');
 const Room = require('./game/Room.js');
 const Messager = require('./message-functions.js');
+const app = express();
+
+
+// ############################
+// # Passport Authentication: #
+// ############################
+
+
+const passport = require('passport')
+  , LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function(err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }
+));
+
+app.post('/login',
+  passport.authenticate('local', { successRedirect: '/',
+                                   failureRedirect: '/login',
+                                   failureFlash: true })
+);
 
 
 // ######################
