09818ac3d4b409aac966db70bd58f9f3cf73d62e
Danny Fekete <dannyfekete@gmail.com>
2018-10-29 15:59:05 -0400

    Added a loud announcement for the winning player at the end of the round in the results modal.

--- client/src/ScoreModal.js
+++ client/src/ScoreModal.js
@@ -5,16 +5,19 @@ class ScoreModal extends Component {
 
   render() {
 
-    if (this.props.gameState === "getResults" && Object.keys(this.props.finalResults).length > 0 ) {
+    if (this.props.gameState === "getResults" && Object.keys(this.props.finalResults).length > 0 && this.props.finalResults.finalScoreboard[0]) {
 
       return (
 
         <div className="modal-backdrop-custom">
           <div className="modal-content">
             <div className="modal-header">
-              <h5 className="modal-title">It's Over!  {this.props.finalResults.finalScoreboard[0].handle} won the round with {this.props.finalResults.finalScoreboard[0].score} points!</h5>
+              <h5 className="modal-title">It's Over!</h5>
             </div>
             <div className="modal-body">
+              <div className="row">
+                <h1>{this.props.finalResults.finalScoreboard[0].handle} won the round with {this.props.finalResults.finalScoreboard[0].score} points!</h1>
+              </div>
               <div className="row">
                 <div className="col-sm-6">
                   <div className="card" style={{color: 'white'}} >
