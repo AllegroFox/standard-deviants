5f0788a4fc1387cb56df062c3a77f7a5d3edffd0
Danny Fekete <dannyfekete@gmail.com>
2018-10-30 14:09:57 -0400

    Labelling changes to the leaderboard.

--- client/src/LeaderBoard.js
+++ client/src/LeaderBoard.js
@@ -9,7 +9,7 @@ class LeaderBoardModal extends Component {
         <div className="modal-dialog modal-dialog-centered modal-lg" role="document">
           <div className="modal-content">
             <div className="modal-header">
-              <h5 className="modal-title" id="exampleModalCenterTitle">Top Scores</h5>
+              <h5 className="modal-title" id="exampleModalCenterTitle">Leaderboard!</h5>
               <button type="button" className="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
               </button>
@@ -17,21 +17,21 @@ class LeaderBoardModal extends Component {
             <div className="modal-body">
               <div class="row">
                 <div class="col-md-6">
-                  All Time, Synonyms
+                  Top-Scoring <em>Synonyms</em> of All Time
                   <LeaderBoardCard leaderBoard={this.props.leaderBoard[0].topScoringSynonyms}/>
                 </div>
                 <div class="col-md-6">
-                  Today, Synonyms
+                  Today's Top-Scoring <em>Synonyms</em>
                   <LeaderBoardCard leaderBoard={this.props.leaderBoard[1].topScoringSynonyms}/>
                 </div>
               </div>
               <div class="row">
                 <div class="col-md-6">
-                  All Time, Rhymes
+                  Top-Scoring <em>Rhymes</em> of All Time
                   <LeaderBoardCard leaderBoard={this.props.leaderBoard[0].topScoringRhymes}/>
                 </div>
                 <div class="col-md-6">
-                  Today, Rhymes
+                  Today's Top-Scoring <em>Rhymes</em>
                   <LeaderBoardCard leaderBoard={this.props.leaderBoard[1].topScoringRhymes}/>
                 </div>
               </div>
