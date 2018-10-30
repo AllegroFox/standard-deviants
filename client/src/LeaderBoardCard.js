9f6481798fd609f74394fc4b0a7cdd1e0cbbc779
Emily McMinn <emily@Emilys-Macbook-Air-2.local>
2018-10-30 14:50:20 -0400

    leaderboard adjustments

--- client/src/LeaderBoardCard.js
+++ client/src/LeaderBoardCard.js
@@ -13,19 +13,18 @@ class LeaderBoardCard extends Component {
     }
 
     return (
-                  <div class="card">
-                    <div class="card-body">
-                      <div class="card-header">
-                        {conditionalContent(this.props.leaderBoard[0])}
-                      </div>
-                      <ul class="list-group list-group-flush">
-                        <li class="list-group-item">
-                        {conditionalContent(this.props.leaderBoard[1])}
+                  <div className="card">
+                    <div className="card-body">
+                      <ul>
+                        <li>
+                          {conditionalContent(this.props.leaderBoard[0])}
                         </li>
-                        <li class="list-group-item">
-                        {conditionalContent(this.props.leaderBoard[2])}
+                        <li>
+                          {conditionalContent(this.props.leaderBoard[1])}
                         </li>
-
+                        <li>
+                          {conditionalContent(this.props.leaderBoard[2])}
+                        </li>
                       </ul>
                     </div>
                   </div>
