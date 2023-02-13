a096fca79508ce3528e4a44ff4601d24a55eb9b5
Danny Fekete <dfekete@postageapp.com>
2023-02-13 15:05:04 -0500

    Express now listens to requests from anywhere

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ const PORT = 3001;
 const server = express()
    // Make the express server serve static assets (html, javascript, css) from the /public folder
   .use(express.static('public'))
-  .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 // Create the WebSockets server
 const wss = new WebSocket.Server({ server });
